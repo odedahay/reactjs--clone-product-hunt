047553982e5f50292434cf82a1f315e1d58840dd
Rodelio <odedahay@gmail.com>
2017-10-28 05:41:08 +0800

    First Upload

---
+++ src/react-components/HomePage/index.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import ProductList from '../Product/ProductList';
+
+import connectToStores from 'alt-utils/lib/connectToStores';
+import ProductStore from '../../stores/ProductStore';
+import Actions from '../../actions';
+
+
+@connectToStores
+class HomePage extends React.Component{
+  constructor() {
+   super();
+  //  this.state = {
+  //    productList: []
+  //  }
+
+  //  Firebase.database().ref('products').on('value', (snapshot) => {
+  //    var products = snapshot.val();
+   //
+  //    this.setState({
+  //      productList: products
+  //    })
+  //  });
+   Actions.getProducts();
+ }
+
+ static getStores(){
+   return[ProductStore];
+ }
+
+ static getPropsFromStores(){
+   return ProductStore.getState();
+ }
+
+  render(){
+    return(
+      <section>
+        <header>
+          <img src="/img/banner.jpeg" width="100%" />
+        </header>
+
+        <section>
+          <section className="container">
+
+            {
+              this.props.products
+              ?
+              <ProductList productList={this.props.products}/>
+              :
+              null
+            }
+
+          </section>
+        </section>
+      </section>
+    );
+  }
+}
+
+export default HomePage;
